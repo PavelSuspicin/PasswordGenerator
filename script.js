a78ecc335d1c5615cb4782f56163a5000f4d976a
Pavel <pavelsuspicin@gmail.com>
2022-05-06 09:37:11 +0300

    06.05.22

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ const numbersEl = document.getElementById('numbers')
 const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 
+generateEl.addEventListener('click', () => {
+    
+})
+
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
-    symbol: getRandomSymbol,
     number: getRandomNumber,
+    symbol: getRandomSymbol,
 }
 
 function getRandomLower() {
@@ -27,47 +32,3 @@ function getRandomSymbol() {
     const symbol = '!@#$%^&*()<>{}[]/,.=-+'
     return symbol[Math.floor(Math.random() * symbol.length)]
 }
-
-generateEl.addEventListener('click', () => {
-    const length = +lengthEl.value
-    const hasLower = lowercaseEl.checked
-    const hasUpper = uppercaseEl.checked
-    const hasNumber = numbersEl.checked
-    const hasSymbol = symbolsEl.checked
-
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
-})
-
-function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = ''
-    const typesCount = lower + upper + number + symbol
-    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter((item) => Object.values(item)[0])
-
-    if (typesCount === 0) {
-        return ''
-    }
-
-    for (let i = 0; i < length; i += typesCount) {
-        typesArr.forEach((type) => {
-            const funcName = Object.keys(type)
-            generatedPassword += randomFunc[funcName]()
-        })
-    }
-    const finalPassword = generatedPassword.slice(0, length)
-    return finalPassword
-}
-
-clipboardEl.addEventListener('click', () => {
-    const textarea = document.createElement('textarea')
-    const password = resultEl.innerText
-
-    if (!password) {
-        return
-    }
-
-    textarea.value = password
-    document.body.appendChild(textarea)
-    textarea.select()
-    document.execCommand('copy')
-    textarea.remove()
-})
