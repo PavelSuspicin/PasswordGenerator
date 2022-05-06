acd77521ed3d031f45a80cb5dd68c96144be9906
Pavel <pavelsuspicin@gmail.com>
2022-05-06 12:29:14 +0300

    06.05.22

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 const resultEl = document.getElementById('result')
 const clipboardEl = document.getElementById('clipboard')
 const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
 const lowercaseEl = document.getElementById('lowercase')
+const uppercaseEl = document.getElementById('uppercase')
 const numbersEl = document.getElementById('numbers')
 const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 
 generateEl.addEventListener('click', () => {
-    
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
 })
 
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter((item) => Object.values(item)[0])
+
+    if (typesCount === 0) {
+        return ''
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach((type) => {
+            let funcName = Object.keys(type)
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+    const result = generatedPassword.slice(0, length)
+    return result
+}
+
+clipboardEl.addEventListener('click', () => {
+    let textarea = document.createElement('textarea')
+    textarea.value = resultEl.innerText
+
+    if (!textarea.value) {
+        return
+    }
+
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+})
 
 const randomFunc = {
     lower: getRandomLower,
